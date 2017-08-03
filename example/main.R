093e2a8bc23056c292e008bf0ef4fd0e733de987
avantikalal <avantikalal1990@gmail.com>
2017-08-03 15:12:36 -0700

    edited main.R

--- example/main.R
+++ example/main.R
@@ -13,7 +13,6 @@ library("data.table")
 library("ggplot2")
 library("gridExtra")
 
-<<<<<<< HEAD:example.R
 # function to plot extracted signatures
 "plotSignatures" <- function( beta, patients_ids = colnames(beta), genomeFreq = TRUE ) {
   
@@ -50,46 +49,9 @@ library("gridExtra")
   grid.arrange(grobs=glist, ncol=ceiling(nrow(beta)/4))
 }
 
-
-# read the data
-clinical_data = read.csv("data/clinical_data.csv")
-load("data/patient.RData")
-=======
-# function to plot the extracted signatures
-"plotSignatures" <- function( beta, patientss_ids, genomeFreq = TRUE ) {
-    
-    # set rownames and colnames
-    if(genomeFreq) {
-        rownames(beta) = c("Background",paste0("S",1:(nrow(beta)-1)))
-    }
-    else {
-        rownames(beta) = paste0("S",1:nrow(beta))
-    }
-    colnames(beta) = patientss_ids
-    
-    # make the plot
-    x = as.data.table(melt(beta))
-    x[,context := paste0(substr(Var2,1,1),".",substr(Var2,7,7))]
-    x[,alt := paste0(substr(Var2,3,3),">",substr(Var2,5,5))]
-    
-    glist = list()
-    
-    for(i in 1:nrow(beta)) {
-        sig = rownames(beta)[i]
-        glist[[i]] = ggplot(x[Var1 == sig]) + 
-        geom_bar(aes(x = context, y = value, fill = alt), stat = "identity", position = "identity") + 
-        facet_wrap(~alt, nrow=1, scales = "free_x") + 
-        theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-        ggtitle(sig) + 
-        theme(legend.position="none")
-    } 
-    grid.arrange(grobs=glist,ncol=ceiling(nrow(beta)/4))
-}
-
 # load the data
 load("data/clinical_data.RData")
 load("data/patients.RData")
->>>>>>> master:example/main.R
 load("data/genome.RData")
 
 # set the number of signatures and lambda to be considered
