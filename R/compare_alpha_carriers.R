7cfd28f3ed5720a4325b9a1e6947ff3992f2afd6
avantikalal <avantikalal1990@gmail.com>
2017-08-14 18:26:32 -0700

    Revised alpha_brca analysis

--- R/compare_alpha_carriers.R
+++ R/compare_alpha_carriers.R
@@ -7,7 +7,7 @@ load("data/patients.RData")
 load("data/clinical_data.RData")
 
 #Choose best alpha from grid search
-best_alpha = signatures_with_genome$grid_search[[4,3]]$alpha
+best_alpha = signatures_with_genome$best_configuration$alpha
 
 #Normalize alpha to sum to 1 for each patient
 alpha_norm = as.data.frame(best_alpha/rowSums(best_alpha))
@@ -16,6 +16,7 @@ alpha_norm$patient = rownames(patients)
 
 #Merge germline data
 alpha_norm = merge(alpha_norm, clinical_data[, c("Sample", "Gene")], by.x = "patient", by.y = "Sample")
+alpha_norm$germline = ifelse(alpha_norm$Gene %in% c("BRCA1", "BRCA2"), "BRCA", "Control")
 
 #Plot normalized alphas for carriers and controls
-ggplot(melt(alpha_norm, id.vars = c(1, 8), variable.name = "signature")) + geom_boxplot(aes(x = signature, y = value, fill = Gene))
+ggplot(melt(alpha_norm[, c(1:7, 9)], id.vars = c(1, 8), variable.name = "signature")) + geom_boxplot(aes(x = signature, y = value, fill = germline))
