fd59be7b1f1ae4e42c4bb8b36d27f7822b689c4d
danro9685 <daniele.ramazzotti@yahoo.com>
2017-10-03 15:48:20 -0700

    adding background based on germline

--- example/main.R
+++ example/main.R
@@ -54,6 +54,7 @@ library("gridExtra")
 # load the data
 load("data/patients.RData")
 load("data/genome.RData")
+load("data/allbg.RData")
 
 # set the number of signatures and lambda to be considered
 K = 2:15
@@ -61,22 +62,43 @@ lambda_values = c(0.01,0.05,0.10,0.15)
 cross_validation_entries = 0.15
 cross_validation_iterations = 5
 num_processes = 10
-my_seed_starring_beta = 93211
+my_seed_starting_beta = 93211
 my_seed_nmfLasso = 74833
 
+# ANALYSIS USING THE GENOME FREQUENCY AS BACKGROUND
+
 # fit the initial betas for each configuration
-initial_betas = startingBetasEstimation(x=patients,K=K,nmf_runs=10,seed=my_seed_starring_beta,verbose=TRUE)
-save(initial_betas,file="data/initial_betas.RData")
+initial_betas_genome = startingBetasEstimation(x=patients,K=K,nmf_runs=10,seed=my_seed_starting_beta,verbose=TRUE)
+save(initial_betas_genome,file="data/initial_betas_genome.RData")
 
 # fit the signatures with the given background noise model
-signatures_nmfLasso = nmfLasso(x=patients,K=K,starting_beta=initial_betas,background_signature=genome$freq,nmf_runs=10,lambda_values=lambda_values,cross_validation_entries=cross_validation_entries,cross_validation_iterations=cross_validation_iterations,iterations=20,max_iterations_lasso=10000,num_processes=num_processes,seed=my_seed_nmfLasso,verbose=TRUE)
-save(signatures_nmfLasso,file="data/signatures_nmfLasso.RData")
+signatures_nmfLasso_genome = nmfLasso(x=patients,K=K,starting_beta=initial_betas_genome,background_signature=genome$freq,nmf_runs=10,lambda_values=lambda_values,cross_validation_entries=cross_validation_entries,cross_validation_iterations=cross_validation_iterations,iterations=20,max_iterations_lasso=10000,num_processes=num_processes,seed=my_seed_nmfLasso,verbose=TRUE)
+save(signatures_nmfLasso_genome,file="data/signatures_nmfLasso_genome.RData")
 
 # plot the resulting signatures
-initial_betas = rbind(signatures_nmfLasso$best_configuration$background_signature,signatures_nmfLasso$best_configuration$starting_beta)
-initial_betas = initial_betas / rowSums(initial_betas)
-plotSignatures(initial_betas,patients_ids=colnames(patients),backgroundFreq=TRUE)
-plotSignatures(signatures_nmfLasso$best_configuration$beta,patients_ids=colnames(patients),backgroundFreq=TRUE)
+initial_betas_genome = rbind(signatures_nmfLasso_genome$best_configuration$background_signature,signatures_nmfLasso_genome$best_configuration$starting_beta)
+initial_betas_genome = initial_betas_genome / rowSums(initial_betas_genome)
+plotSignatures(initial_betas_genome,patients_ids=colnames(patients),backgroundFreq=TRUE)
+plotSignatures(signatures_nmfLasso_genome$best_configuration$beta,patients_ids=colnames(patients),backgroundFreq=TRUE)
 
 # plot the log-likelihood values
-plot(signatures_nmfLasso$best_configuration$loglik_progression)
+plot(signatures_nmfLasso_genome$best_configuration$loglik_progression)
+
+# ANALYSIS USING THE GERMLINE SIGNATURE AS BACKGROUND
+
+# fit the initial betas for each configuration
+initial_betas_germline = initial_betas_genome
+save(initial_betas_germline,file="data/initial_betas_germline.RData")
+
+# fit the signatures with the given background noise model
+signatures_nmfLasso_germline = nmfLasso(x=patients,K=K,starting_beta=initial_betas_germline,background_signature=allbg$freq,nmf_runs=10,lambda_values=lambda_values,cross_validation_entries=cross_validation_entries,cross_validation_iterations=cross_validation_iterations,iterations=20,max_iterations_lasso=10000,num_processes=num_processes,seed=my_seed_nmfLasso,verbose=TRUE)
+save(signatures_nmfLasso_germline,file="data/signatures_nmfLasso_germline.RData")
+
+# plot the resulting signatures
+initial_betas_germline = rbind(signatures_nmfLasso_germline$best_configuration$background_signature,signatures_nmfLasso_germline$best_configuration$starting_beta)
+initial_betas_germline = initial_betas_germline / rowSums(initial_betas_germline)
+plotSignatures(initial_betas_germline,patients_ids=colnames(patients),backgroundFreq=TRUE)
+plotSignatures(signatures_nmfLasso_germline$best_configuration$beta,patients_ids=colnames(patients),backgroundFreq=TRUE)
+
+# plot the log-likelihood values
+plot(signatures_nmfLasso_germline$best_configuration$loglik_progression)
