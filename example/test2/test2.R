056142c602b76f7886eac3dec21803b1c1437292
danro9685 <daniele.ramazzotti@yahoo.com>
2017-10-21 09:39:27 -0700

    externsive iteration fit

--- example/test2/test2.R
+++ example/test2/test2.R
@@ -208,3 +208,28 @@ plotSignatures(nmf_results$lasso[[4]]$beta)
 plotSignatures(nmf_results$lasso[[5]]$beta)
 plotSignatures(nmf_results$lasso[[6]]$beta)
 plotSignatures(nmf_results$lasso[[7]]$beta)
+
+# make the final inference for the best configurations
+i = 3
+set.seed(43214)
+curr_starting_beta = initial_betas_germline_cv_10[[i,1]]
+curr_res = nmfLassoDecomposition(x=patients,beta=rbind(genome_background,curr_starting_beta),lambda_rate=0.15,iterations=100,verbose=TRUE)
+curr_alpha_nmf_lasso = curr_res$alpha
+curr_beta_nmf_lasso = curr_res$beta
+curr_predicted_counts = round(curr_alpha_nmf_lasso%*%curr_beta_nmf_lasso)
+curr_mse = sum((patients-curr_predicted_counts)^2)/nrow(patients) # 104332.7
+curr_mse_loss = MSE(curr_predicted_counts,as.matrix(patients)) # 1086.799
+plot(curr_res$loglik_progression)
+plotSignatures(curr_res$beta)
+
+i = 4
+set.seed(65711)
+curr_starting_beta = initial_betas_germline_cv_10[[i,1]]
+curr_res = nmfLassoDecomposition(x=patients,beta=rbind(genome_background,curr_starting_beta),lambda_rate=0.15,iterations=100,verbose=TRUE)
+curr_alpha_nmf_lasso = curr_res$alpha
+curr_beta_nmf_lasso = curr_res$beta
+curr_predicted_counts = round(curr_alpha_nmf_lasso%*%curr_beta_nmf_lasso)
+curr_mse = sum((patients-curr_predicted_counts)^2)/nrow(patients) # 44268.6
+curr_mse_loss = MSE(curr_predicted_counts,as.matrix(patients)) # 461.1313
+plot(curr_res$loglik_progression)
+plotSignatures(curr_res$beta)
