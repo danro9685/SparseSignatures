fc54edf2dc39d868dfeaea2a0cb634d4b8ecd31f
avantikalal <avantikalal1990@gmail.com>
2018-03-15 13:50:53 -0700

    Added function to plot beta matrix of signatures

--- R/plotPatient.R
+++ R/plotPatient.R
@@ -2,23 +2,38 @@ library(data.table)
 library(ggplot2)
 
 #Plot patient counts
-plotPatient = function(countMatrix, patientName, xlabels=T){
+plotPatient = function(countMatrix, patientName, xlabels=T, freq=F){
   
   #Separate contexts and alterations
-  x = as.data.table(melt(countMatrix[patientName,]))
-  x[, context := paste0(substr(variable, 1,1), ".", substr(variable, 7,7))]
-  x[, alt := paste0(substr(variable, 3,3), ">", substr(variable, 5,5))]
+  x = as.data.table(suppressWarnings(melt(countMatrix[patientName,], variable.name = "cat", value.name = "N")))
+  x[, Context := paste0(substr(cat, 1,1), ".", substr(cat, 7,7))]
+  x[, alt := paste0(substr(cat, 3,3), ">", substr(cat, 5,5))]
+  
+  #Convert to frequency if needed
+  if(freq){
+    x[, freq:=N/x[, sum(N)]]
+  }
   
   #Plot
-  plt = ggplot(x) + 
-    geom_bar(aes(x = context, y = value, fill = alt), stat = "identity", position = "identity") + 
-    facet_wrap(~alt, nrow=1, scales = "free_x") + 
-    theme(axis.text.x = element_text(angle = 90, hjust = 1), panel.background = element_blank(), axis.line = element_line(colour = "black"))+
-    theme(legend.position="none")+ggtitle(patientName)
-  
+  if(freq){
+    plt = ggplot(x) + 
+      geom_bar(aes(x = Context, y = freq, fill = alt), stat = "identity", position = "identity") + 
+      facet_wrap(~alt, nrow=1, scales = "free_x") + 
+      theme(axis.text.x = element_text(angle = 90, hjust = 1), panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+      theme(legend.position="none")+ggtitle(patientName)+ylab("Frequency of mutations")
+  }else{
+    plt = ggplot(x) + 
+      geom_bar(aes(x = Context, y = N, fill = alt), stat = "identity", position = "identity") + 
+      facet_wrap(~alt, nrow=1, scales = "free_x") + 
+      theme(axis.text.x = element_text(angle = 90, hjust = 1), panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+      theme(legend.position="none")+ggtitle(patientName)+ylab("Number of mutations")
+  }
+
   if(!xlabels){
     plt = plt+theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
   }
+  
+
   
   return(plt)
   
