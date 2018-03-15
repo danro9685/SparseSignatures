dbd443da785b290ff8b26c4bf07fcd6c822e7161
danro9685 <daniele.ramazzotti@yahoo.com>
2018-03-14 17:59:16 -0700

    packaging

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' @name patients 
+#' @title point mutations for 560 breast tumors 
+#' @description dataset of counts of the point mutations detected in 560 breast tumors published in Nik-Zainal, Serena, et al. (2016). 
+#' @docType data 
+#' @usage data(patients) 
+#' @format counts of the point mutations 
+#' @source Nik-Zainal, Serena, et al. "Landscape of somatic mutations in 560 breast cancer whole-genome sequences." Nature 534.7605 (2016): 47. 
+#' @return counts of point mutations for 560 tumors and 96 trinucleotides 
+NULL
+
+#' @name background 
+#' @title germline replication error 
+#' @description germline replication error estimated in Rahbari, Raheleh, et al. (2016). 
+#' @docType data 
+#' @usage data(background) 
+#' @format vector of rates 
+#' @source Rahbari, Raheleh, et al. "Timing, rates and spectra of human germline mutation." Nature genetics 48.2 (2016): 126. 
+#' @return vector of rates for the 96 trinucleotides 
+NULL
