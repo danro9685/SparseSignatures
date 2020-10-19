d751f4736efcf7639281b83900cc6b13fc260a71
danro9685 <daniele.ramazzotti@yahoo.com>
2020-10-19 18:04:59 +0200

    pages setup

--- tests/testthat/test_algorithm.R
+++
@@ -1,6 +0,0 @@
-context("SparseSignatures")
-
-data("nmf_LassoK_example")
-test_that("SparseSignatures produces correct output", {
-    expect_equal(names(nmf_LassoK_example),c("alpha","beta","starting_beta","best_loglik","loglik_progression"))
-})
